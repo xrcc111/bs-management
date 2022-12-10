5852fc220e38bf8087db55f51db8bcdb5e8a6a0e
徐锐 <2603029264@qq.com>
2022-12-10 15:15:37 +0800

    init bs-mangement

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+
+import styles from './header.module.scss' // 模块化引入
+
+export default function Header() {
+  return (
+    <div className={styles.title}>Header</div>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path' // npm install @types/node -D 解决报红问题
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
